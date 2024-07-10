7705d32e2064c39f2c873872e67b8ca4d19e85f6
souraOP <sourasishmondal95@gmail.com>
2024-07-10 19:00:24 +0530

    changing the file

--- ratCountHouse.java
+++ ratCountHouse.java
@@ -32,8 +32,39 @@ Note:
 
 */
 
+import java.util.*;
 class ratCountHouse {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int unit = sc.nextInt();
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(solve(arr, r, unit, n));
+        sc.close();
+    }
+    
+    public static int solve(int[] arr, int r, int unit, int n) {
+        int sum = 0;
+        if(arr == null) {
+            return -1;
+        }
+        int count = 0;
+        int ans = r * unit;
+        for(int i =0; i < n; i++) {
+            sum += arr[i];  // getting the sums of all the houses
+            count++;
+            if(sum >= ans) {
+                break;
+            }
+        }
+        if (sum < ans) {
+            return 0;
+        }
         
+        return count;
     }
 }
